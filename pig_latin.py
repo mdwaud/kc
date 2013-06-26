d89999fd7ca0e773fe5f371caca3bcfb17226b39
Mike <mike@Michaels-iMac.local>
2013-06-26 14:03:48 -0400

    - added some comments

--- pig_latin.py
+++ pig_latin.py
@@ -14,12 +14,14 @@ def pig_latinize(word):
     >>> pig_latinize("amber")
     'amberay'
     """
+    # handle some simple base cases
     if len(word) < 3 or word.upper() in IGNORE_LIST:
         return word
-
+    # cehck for the first vowel
     for index, letter in enumerate(word):
         if letter.upper() in VOWELS:
             return word[index:] + word[:index] + "ay"
+    # no vowels, so just return what we got
     return word
 
 
