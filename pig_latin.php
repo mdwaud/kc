ef7a860b13f46fc0d0a15158a93eefdf7896a3e1
Mike <mike@Michaels-iMac.local>
2013-06-26 18:22:44 -0400

    - changed handling of string case - matched rules to wikipedia

--- pig_latin.php
+++ pig_latin.php
@@ -1,33 +1,29 @@
 <?php
 
-define('LETTER_LIMIT', 3);
-
-function pig_latinize($word) {
+function pig_latinize($raw_word) {
     /*
      *  Return a pig latin-ized version of a word
      */
-    $VOWELS = array("A","E","I","O","U",);
-    $IGNORE_LIST = array("THE",);
-    // check some simple base cases
-    if (strlen($word) < LETTER_LIMIT || in_array(strtoupper($word), $IGNORE_LIST)) {
-        return $word;
-    }
+    $VOWELS = array("a","e","i","o","u","y");
+    $word = strtolower($raw_word);
     // check for the first vowel
     $index = 0;
     foreach(str_split($word) as $letter) {
-        if(in_array(strtoupper($letter), $VOWELS)) {
-            return substr($word, $index, strlen($word)) . substr($word, 0, $index) . "ay";
+        if(in_array($letter, $VOWELS)) {
+            if($index > 0) {
+                return substr($word, $index, strlen($word)) . substr($word, 0, $index) . "ay";
+            } else {
+                return $word . "yay";
+            }
         }
         $index++;
     }
-    # no vowels, so just return what we got
-    return $word;
+    # no vowels... let's just add an "ay"
+    return $word . "ay";
 }
 
-assert('pig_latinize("Computer") == "omputerCay"');
-assert('pig_latinize("Crevice") == "eviceCray"');
-assert('pig_latinize("My") == "My"');
-assert('pig_latinize("the") == "the"');
-assert('pig_latinize("amber") == "amberay"');
+assert('pig_latinize("happy") == "appyhay"');
+assert('pig_latinize("duck") == "uckday"');
+assert('pig_latinize("egg") == "eggyay"');
 
 ?>
--- pig_latin.py
+++ pig_latin.py
@@ -1,30 +1,28 @@
-VOWELS = "AEIOUY"
-IGNORE_LIST = ["THE",]
+VOWELS = "aeiouy"
 
-def pig_latinize(word):
+def pig_latinize(raw_word):
     """
     Return a pig latin-ized version of a word
 
-    >>> pig_latinize("Python")
-    'ythonPay'
-    >>> pig_latinize("The")
-    'The'
-    >>> pig_latinize("my")
-    'my'
-    >>> pig_latinize("amber")
-    'amberay'
-    >>> pig_latinize("crevice")
-    'evicecray'
+    >>> pig_latinize("happy")
+    'appyhay'
+    >>> pig_latinize("duck")
+    'uckday'
+    >>> pig_latinize("egg")
+    'eggyay'
+    >>> pig_latinize("thbbbt")
+    'thbbbtay'
     """
-    # handle some simple base cases
-    if len(word) < 3 or word.upper() in IGNORE_LIST:
-        return word
+    word = raw_word.lower()
     # check for the first vowel
     for index, letter in enumerate(word):
-        if letter.upper() in VOWELS:
-            return word[index:] + word[:index] + "ay"
-    # no vowels, so just return what we got
-    return word
+        if letter in VOWELS:
+            if index > 0:
+                return word[index:] + word[:index] + "ay"
+            else:
+                return word + "yay"
+    # no vowels... let's just add an "ay"
+    return word + "ay"
 
 
 if __name__ == "__main__":
